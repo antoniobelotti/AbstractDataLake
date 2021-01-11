755e21dbfddaea4a5635d4e5a379bdb49714770b
antonio <antonio.belotti@studenti.unimi.it>
2021-01-11 19:00:30 +0100

    refactor interface methods

--- AbstractJSONDataLakeConnector/__init__.py
+++ AbstractJSONDataLakeConnector/__init__.py
@@ -14,41 +14,31 @@ class JSONDataLakeConnection(ABC):
         pass
 
     @abstractmethod
-    def pwd(self) -> str:
-        """ Returns the current remote data lake path """
-        pass
-
-    @abstractmethod
-    def cd(self, path: str):
-        """ Change current path. Will raise error if the specified path does not exist """
-        pass
-
-    @abstractmethod
     def mkdir(self, path: str):
-        """ Create new empty directory inside current working directory. Absolute path not supported. """
+        """ Create new empty directory """
         pass
 
     @abstractmethod
-    def rmdir(self, path: str):
-        """ Delete specified directory and all its content. Target directory has to be inside the current working directory. Absolute path not supported."""
+    def rmdir(self, path: str, recursive: bool = True):
+        """ Delete specified directory and all its content. RECURSIVE BY DEFAULT: set recursive=False to raise an error in case the directory has content """
         pass
 
     @abstractmethod
-    def store(self, serialized_json_content: str, filename: str, overwrite: bool):
-        """ Store a file inside the current working directory """
+    def store(self, serialized_json_content: str, path_filename: str, overwrite: bool):
+        """ Upload a file """
         pass
 
     @abstractmethod
-    def retrieve(self, filename: str) -> dict:
-        """ Retrieve a file from the current working directory """
+    def retrieve(self, path_filename: str) -> dict:
+        """ Retrieve a file """
         pass
 
     @abstractmethod
-    def rm(self, filename: str):
-        """ Remove specified file from the current working directory """
+    def rm(self, path_filename: str):
+        """ Delete file """
         pass
 
     @abstractmethod
-    def ls(self) -> [str]:
-        """ Returns a list of the content of the current working directory """
+    def ls(self, path:str) -> [str]:
+        """ Returns a list of the content of the specified directory """
         pass
